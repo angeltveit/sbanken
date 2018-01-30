6eb516c70789c341ca985b25394f34b7d2608042
Torbjorn Angeltveit <torbjorn.angeltveit@scoutgaminggroup.com>
2018-01-30 11:50:11 +0100

    print todays transactions

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const { getAccessToken, getTodaysTransactions } = require('./lib')
+const { getAccessToken, getAccounts ,getTodaysTransactions } = require('./lib')
 const moment = require('moment')
 
 async function run() {
   const accessToken = await getAccessToken()
   const today = await getTodaysTransactions(accessToken)
 
+
   let total = 0
   today.forEach(item=> {
     if(moment().isSame(item.registrationDate, 'day')) {
       total += Math.abs(item.amount)
+      console.log(`${item.amount} - ${item.text}`)
     }
   })
 
